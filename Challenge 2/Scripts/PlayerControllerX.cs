79cadcd8e398a1ca6be89e820678a59eadd5b0b7
gamedevJed <109969084+gamedevJed@users.noreply.github.com>
2022-08-02 13:30:07 -0500

    Add files via upload

---
+++ Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float dogCoolDown = 1.0f;
+    private float dogTimer;
+
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // updates the timer 
+        dogTimer += Time.deltaTime;
+
+        // On spacebar press, send dog , check to make time duration has been satisfied
+        if (Input.GetKeyDown(KeyCode.Space) && dogTimer > dogCoolDown)
+        {
+            
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            // reset the dog timer.
+            dogTimer = 0.0f;
+        }
+    }
+}
---
+++ Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBoundary = 30;
+    private float bottomBoundary = -10;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topBoundary)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < bottomBoundary)
+        {
+            Debug.Log("Game Over");
+            Destroy(gameObject);
+        }
+    }
+}
